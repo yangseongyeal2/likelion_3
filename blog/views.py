296a9aaf41f1080d109a7807954b2ebf050a708d
yangseongyeal2 <61301184+yangseongyeal@users.noreply.github.com>
2020-08-03 12:03:38 +0900

    secondproject

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Blog
+
+def home(request):
+    blogs=Blog.objects
+    return render(request,'home.html',{'blogs':blogs})
+
+# Create your views here.
